89efb361ac37066d8392cf6cd5f6c60fa5be7060
Ivan Voznyi <ivan.voznyi92@gmail.com>
2025-12-29 09:37:46 +0200

    Initial Commit

---
+++ Particle_Wave_Simulation/Particle_Wave_SimulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Particle_Wave_SimulationApp.swift
+//  Particle_Wave_Simulation
+//
+//  Created by Ivan Voznyi on 12/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Particle_Wave_SimulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
